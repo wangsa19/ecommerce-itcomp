14f66383d83f7c4a88d200eb41b2b2bccf926048
wangsa19 <dewanggawpw@gmail.com>
2025-02-21 23:32:38 +0700

    Add Widget in OrderResource and Tab

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
 
 class ListOrders extends ListRecords
 {
@@ -15,5 +17,24 @@ class ListOrders extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class,
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All'),
+            'new' => Tab::make()->query(fn ($query) => $query->where('status', 'new')),
+            'processing' => Tab::make()->query(fn ($query) => $query->where('status', 'processing')),
+            'shipped' => Tab::make()->query(fn ($query) => $query->where('status', 'shipped')),
+            'delivered' => Tab::make()->query(fn ($query) => $query->where('status', 'delivered')),
+            'cancelled' => Tab::make()->query(fn ($query) => $query->where('status', 'cancelled')),
+        ] ;
     }
 }
